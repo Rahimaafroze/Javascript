504f68e7e32412f892dc1ea33943003016c1dd13
Rahimaafroze <ritumoni170@gmail.comm>
2022-02-02 00:07:03 +0600

    Vriables,Data Types

--- variable.js
+++ variable.js
@@ -1,3 +1,27 @@
-var num1 = "99.5";
-var num2 = 15;
-console.log(num1 + num2);
+// variable declaration using *var* keyword
+
+/* var firstName = "Rahima"; //anything between "" or '' is called string
+var secondName = "Afroze";
+var fullName = firstName + " " + secondName;
+console.log(fullName); */
+
+//variable declaration using *let* keyword
+
+/* let mangoPrice = 30;
+let milkPrice = 80;
+let sugarPrice = 40;
+sugarPrice += 30; //increment by 1(++),decrement by 1(--),shorthand: += ,-=  
+console.log(sugarPrice); */
+
+
+//variable declaration using *cons* keyword 
+
+/* const pi = 3.1416;
+console.log(pi); */
+
+//String Type
+
+let herName = 'Ritu'; //String literal
+let myName = String('Afroze'); //Factory function
+let hisName = new String('Rahim'); //Constructor
+console.log(myName);
